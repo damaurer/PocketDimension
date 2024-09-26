5132ca5354855461d1f5304add1ead6197753b4d
Maurer Michael <michael.maurer1991@outlook.de>
2024-09-26 17:08:14 +0200

    Switch to Node adapter, update favicon, remove Sverdle link
    Changed the SvelteKit adapter to Node for backend deployment. Updated the favicon link to SVG format for better resolution. Removed the Sverdle page link from the About page for a cleaner user interface.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+
 </script>
 
 <svelte:head>
@@ -13,8 +11,7 @@
 	<h1>
 		<span class="welcome">
 			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
+
 			</picture>
 		</span>
 
@@ -25,7 +22,6 @@
 		try editing <strong>src/routes/+page.svelte</strong>
 	</h2>
 
-	<Counter />
 </section>
 
 <style>
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,48 +1,83 @@
-"use strict";
-// Cache Name
-const CACHE_NAME = "static-cache-v1";
-// Cache Files
-const FILES_TO_CACHE = ["/offline.html"];
-// install
-self.addEventListener("install", (evt) => {
-    console.log("[ServiceWorker] Install");
-    evt.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            console.log("[ServiceWorker] Pre-caching offline page");
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
-    self.skipWaiting();
+
+/// <reference types="@sveltejs/kit" />
+import { build, files, version } from '$service-worker';
+
+
+
+// Create a unique cache name for this deployment
+const CACHE = `pd-cache-${version}`;
+
+const ASSETS = [
+  ...build, // the app itself
+  ...files  // everything in `static`
+];
+
+self.addEventListener('install', (event) => {
+  // Create a new cache and add all files to it
+  async function addFilesToCache() {
+    const cache = await caches.open(CACHE);
+    await cache.addAll(ASSETS);
+  }
+
+  event.waitUntil(addFilesToCache());
 });
-// Active PWA Cache and clear out anything older
-self.addEventListener("activate", (evt) => {
-    console.log("[ServiceWorker] Activate");
-    evt.waitUntil(
-        caches.keys().then((keyList) => {
-            return Promise.all(
-                keyList.map((key) => {
-                    if (key !== CACHE_NAME) {
-                        console.log("[ServiceWorker] Removing old cache", key);
-                        return caches.delete(key);
-                    }
-                })
-            );
-        })
-    );
-    self.clients.claim();
-});
-// listen for fetch events in page navigation and return anything that has been cached
-self.addEventListener("fetch", (evt) => {
-    console.log("[ServiceWorker] Fetch", evt.request.url);
-    // when not a navigation event return
-    if (evt.request.mode !== "navigate") {
-        return;
+
+self.addEventListener('activate', (event) => {
+  // Remove previous cached data from disk
+  async function deleteOldCaches() {
+    for (const key of await caches.keys()) {
+      if (key !== CACHE) await caches.delete(key);
     }
-    evt.respondWith(
-        fetch(evt.request).catch(() => {
-            return caches.open(CACHE_NAME).then((cache) => {
-                return cache.match("offline.html");
-            });
-        })
-    );
+  }
+
+  event.waitUntil(deleteOldCaches());
+});
+
+self.addEventListener('fetch', (event) => {
+  // ignore POST requests etc
+  if (event.request.method !== 'GET') return;
+
+  async function respond() {
+    const url = new URL(event.request.url);
+    const cache = await caches.open(CACHE);
+
+    // `build`/`files` can always be served from the cache
+    if (ASSETS.includes(url.pathname)) {
+      const response = await cache.match(url.pathname);
+
+      if (response) {
+        return response;
+      }
+    }
+
+    // for everything else, try the network first, but
+    // fall back to the cache if we're offline
+    try {
+      const response = await fetch(event.request);
+
+      // if we're offline, fetch can return a value that is not a Response
+      // instead of throwing - and we can't pass this non-Response to respondWith
+      if (!(response instanceof Response)) {
+        throw new Error('invalid response from fetch');
+      }
+
+      if (response.status === 200) {
+        cache.put(event.request, response.clone());
+      }
+
+      return response;
+    } catch (err) {
+      const response = await cache.match(event.request);
+
+      if (response) {
+        return response;
+      }
+
+      // if there's no cache, then just error out
+      // as there is nothing we can do to respond to this request
+      throw err;
+    }
+  }
+
+  event.respondWith(respond());
 });
