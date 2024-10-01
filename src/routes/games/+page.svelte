253bfb305599db0516a29f6fc049ca551ce9e06f
Maurer Michael <michael.maurer1991@outlook.de>
2024-10-01 18:23:02 +0200

    Add "Games" translation and new "Games" page
    Added translations for the "Games" term in both English and German. Created a new "Games" page with static content and enabled prerendering for better performance.

---
+++ src/routes/games/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Translate from '$lib/i18n/Translate.svelte';
+</script>
+
+<svelte:head>
+	<title>Games</title>
+	<meta name="description" content="Games" />
+</svelte:head>
+
+<div class="text-column">
+	<h1><Translate key="games"/></h1>
+
+</div>
