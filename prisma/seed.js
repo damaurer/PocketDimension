82579569ad0bf7976ac13d588f4bbe5604208d29
Maurer Michael <michael.maurer1991@outlook.de>
2024-10-04 10:21:58 +0200

    Add role seeding and environment setup for dev
    Added initial role seeding for 'ADMINISTRATOR_ROLE' and 'USER_ROLE' in the Prisma seed file. Updated Prisma schema to ensure role names are unique. Added environment variable configuration for development mode in dev.run.xml and created a new run configuration for Prisma seed.

--- prisma/seed.js
+++ prisma/seed.js
@@ -6,6 +6,32 @@ const prisma = new PrismaClient();
 async function main() {
 	const { DB_ADMIN_USER, DB_ADMIN_EMAIL, DB_ADMIN_PASSWORD, DB_SHOW_ADMIN_PASSWORD } = process.env;
 
+	const roles = [];
+
+	roles.push(await prisma.role.upsert({
+		create: {
+			name: 'ADMINISTRATOR_ROLE'
+		},
+		update: {
+			name: 'ADMINISTRATOR_ROLE'
+		},
+		where: {
+			name: 'ADMINISTRATOR_ROLE'
+		}
+	}));
+	roles.push(await prisma.role.upsert({
+		create: {
+			name: 'USER_ROLE'
+		},
+		update: {
+			name: 'USER_ROLE'
+		},
+		where: {
+			name: 'USER_ROLE'
+		}
+	}));
+
+
 	const user = await prisma.user.findUnique({
 		where: {
 			email: DB_ADMIN_EMAIL
@@ -17,14 +43,46 @@ async function main() {
 			data: {
 				name: DB_ADMIN_USER,
 				email: DB_ADMIN_EMAIL,
-				password: await bcrypt.hash(DB_ADMIN_PASSWORD, 10)
+				password: await bcrypt.hash(DB_ADMIN_PASSWORD, 10),
+				roles: {
+					create: roles.map((r) => ({
+						role: {
+							connect: {
+								name: r.name
+							}
+						}
+					}))
+				}
 			}
 		});
 
 		if (DB_SHOW_ADMIN_PASSWORD) {
-			console.log({ admin: {...admin, password: DB_ADMIN_PASSWORD} });
-		}else {
-			console.log({admin})
+			console.log({ admin: { ...admin, password: DB_ADMIN_PASSWORD } });
+		} else {
+			console.log({ admin });
+		}
+	}
+
+	if (process.env.NODE_ENV === 'development') {
+		for(let i = 0; i < 20; i++) {
+			await prisma.user.create({
+				data: {
+					name: `User${i}`,
+					email: `user${i}@testuser.de`,
+					password: await bcrypt.hash(`testuserUser${i}`, 10),
+					roles: {
+						create: [
+							{
+								role: {
+									connect: {
+										name: 'USER_ROLE'
+									}
+								}
+							}
+						]
+					}
+				}
+			});
 		}
 	}
 }
